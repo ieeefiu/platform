62377c6c636bd4c0a12f65a9d35e5dfee2bdf2b2
William Oberndorfer <will.oberndorfer@gmail.com>
2016-02-20 20:36:21 -0500

    start implementing pin functions

--- src/pins.c
+++ src/pins.c
@@ -4,12 +4,13 @@
 
 #include "pins.h"
 
-Pin *pin_new(uint8_t channel)
+Pin *pin_new(uint8_t channel, uint8_t zero_pin)
 {
     Pin *pin;
     pin = malloc(sizeof(Pin));
 
     pin->channel = channel;
+    pin->zero = zero_pin;
     
     return pin;
 }
@@ -19,22 +20,55 @@ void pin_init(Pin *pin)
     
 }
 
-uint8_t pin_return_to_zero(Pin *pin)
+uint8_t pins_return_to_zero(Pin **pin)
 {
+    uint8_t stop_status = 0;
+    int i;
+    while (stop_status < SENSOR_NUMBER) {
+        stop_status = 0;
+        for (i = 0; i < SENSOR_NUMBER; i++) {
+            if (pin_check_zero(pin[i])) {
+                pin_halt(pin[i]);
+                stop_status++;
+            }
+            else {
+                pin_move_backwards(pin[i]);
+            }
+        }
+    } 
     return 0;
 }
 
 uint8_t pin_move_forward(Pin *pin)
 {
+    // tell motor to move forward
     return 0;
 }
 
 uint8_t pin_move_backwards(Pin *pin)
 {
+    if (pin_check_zero(pin)) {
+        pin_halt(pin);
+        return 1;
+    }
+    else {
+        // tell motor to move backwards
+    }
     return 0;
+}
+
+uint8_t pin_check_zero(Pin *pin)
+{
+    if (bit_is_clear(PINB, pin->zero)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 uint8_t pin_halt(Pin *pin)
 {
+    // tell motor to stop
     return 0;
 }
--- src/pins.h
+++ src/pins.h
@@ -15,22 +15,26 @@
 typedef struct {
     /* placeholder for motor */
     uint8_t channel;
+    uint8_t zero;
 } Pin;
 
 /* make new pin */
-Pin *pin_new(uint8_t channel);
+Pin *pin_new(uint8_t channel, uint8_t zero_pin);
 
 /* initialize pin */
 void pin_init(Pin *pin);
 
 /* reset rail to zero position */
-uint8_t pin_return_to_zero(Pin *pin);
+uint8_t pins_return_to_zero(Pin **pin);
 
 /* tell pin to move forward */
 uint8_t pin_move_forward(Pin *pin);
 
 /* tell pin to move backwards */
 uint8_t pin_move_backwards(Pin *pin);
+
+/* checks to see if pin is at zero position */
+uint8_t pin_check_zero(Pin *pin);
 
 /* halt pin */
 uint8_t pin_halt(Pin *pin);
